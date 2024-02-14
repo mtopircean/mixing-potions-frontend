4fadd834ff625fee13707d0e23f6296518eaf432
Marius Topircean <mtopircean@yahoo.com>
2024-02-14 21:00:55 +0000

    Create NavBar app and basic elements added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import { Container } from "react-bootstrap";
+import "./App.css";
+import { Navbar, Nav } from "react-bootstrap";
+import logo from "../src/assets/logo.png";
 
 function App() {
   return (
-    <div className="App">
-       
-    </div>
+    <Container>
+      <Navbar expand="md" fixed="top">
+        <Navbar.Brand href="#home">
+          <div className="logo-container">
+            <img src={logo} alt="logo" height="120" />
+          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Container>
   );
 }
 
