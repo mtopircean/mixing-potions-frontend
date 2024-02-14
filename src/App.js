60a0289d7f522d4a59d9ac31db820978459d78cb
Marius Topircean <mtopircean@yahoo.com>
2024-02-14 22:39:53 +0000

    Create navbar, menu, add logo and apply basic css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import { Container } from "react-bootstrap";
 import "./App.css";
-import { Navbar, Nav } from "react-bootstrap";
-import logo from "../src/assets/logo.png";
+import NavBar from "../src/components/NavBar";
 
 function App() {
   return (
-    <Container>
-      <Navbar expand="md" fixed="top">
-        <Navbar.Brand href="#home">
-          <div className="logo-container">
-            <img src={logo} alt="logo" height="120" />
-          </div>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </Container>
+    <div className="App">
+      <NavBar />
+    </div>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Container, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faBars } from '@fortawesome/free-solid-svg-icons';
 
-const NavBar = () => {
+function App() {
   return (
-    <div>NavBar</div>
-  )
+    <Container>
+      <Navbar expand="md" fixed="top" className="navbar-container">
+        <Navbar.Brand href="#home">
+          <div className="logo-container">
+            <img src={logo} alt="logo" height="120" />
+          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+          <NavDropdown title={<FontAwesomeIcon icon={faBars} />} id="basic-nav-dropdown">
+          <div id="menu-items">
+          <NavDropdown.Item href="#">Home</NavDropdown.Item>
+          <NavDropdown.Item href="#">About</NavDropdown.Item>
+          <NavDropdown.Item href="#">Join our team</NavDropdown.Item>
+          <NavDropdown.Item href="#">Contact</NavDropdown.Item>
+          </div>
+          </NavDropdown>
+            <Nav.Link href="#link">Join/Login <FontAwesomeIcon icon={faUser} /></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Container>
+  );
 }
 
-export default NavBar
+export default App;
