d59fa19fb3117afc63a753b618f2316403648b5f
Marius Topircean <mtopircean@yahoo.com>
2024-02-15 20:05:47 +0000

    Create footer links to internal and external pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,9 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import {
   faWhatsapp,
   faFacebook,
-  faTwitter,
+  faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
+import { NavLink } from "react-router-dom";
 
 function App() {
   return (
@@ -17,26 +18,35 @@ function App() {
         <Row>
           <Col xs={4} className="text-left">
             <div className="gdpr">
-              <a href="#">GDPR</a>
+              <NavLink to="gdpr">GDPR</NavLink>
             </div>
           </Col>
           <Col xs={4} className="text-center">
             <div className="join">
-              <a href="#">
+              <a
+                href="https://beta-doterra.myvoffice.com/cosminatopircean/#/contactUs"
+                target="blank"
+              >
                 Join Us <FontAwesomeIcon icon={faPlus} />
               </a>
             </div>
           </Col>
           <Col xs={4} className="text-right">
             <div className="social">
-              <a href="#">
+              <a href="https://wa.me/+353876403084" target="_blank">
                 <FontAwesomeIcon icon={faWhatsapp} />
               </a>
-              <a href="#">
+              <a
+                href="https://www.facebook.com/gherasim.cosmina"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faFacebook} />
               </a>
-              <a href="#">
-                <FontAwesomeIcon icon={faTwitter} />
+              <a
+                href="https://www.instagram.com/cosminatopircean/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA%3D%3D"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faInstagram} />
               </a>
             </div>
           </Col>
