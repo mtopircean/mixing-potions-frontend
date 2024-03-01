c6ef26a613c325aebc51542c78fc62e590a2432a
Marius Topircean <mtopircean@yahoo.com>
2024-03-01 15:50:44 +0000

    Implement tickbox and function to handle tickbox setting product photo as post main photo

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -10,6 +10,7 @@ function PostCreateForm() {
   const [selectedBodySystems, setSelectedBodySystems] = useState([]);
   const [selectedProducts, setSelectedProducts] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const toggleBodySystem = (system) => {
     if (selectedBodySystems.includes(system)) {
@@ -44,81 +45,95 @@ function PostCreateForm() {
     );
   };
 
+  const handleCheckboxChange = (product) => {
+    setSelectedImage(product.image);
+  }
+
   return (
     <Container>
       <Row>
         <Col sm={6}>
-          <Form>
-            <Row className="align-items-center justify-content-center">
-              {selectedProducts.length > 2 && currentIndex > 0 && (
-                <Button
-                  onClick={handlePrevClick}
-                  className={styles["arrow-button"]}
-                >
-                  <FaChevronLeft />
-                </Button>
-              )}
-              {selectedProducts
-                .slice(currentIndex, currentIndex + 2)
-                .map((product) => (
-                  <Col key={product.id} sm={4}>
-                    <div
-                      key={product.id}
-                      className={styles["product-post-card"]}
-                    >
-                      <Image src={product.image} alt={product.name} fluid />
-                      <h5>{product.name}</h5>
-                      <Button
-                        variant="danger"
-                        onClick={() => handleRemoveProduct(product.id)}
-                        className={styles["remove-button"]}
-                      >
-                        <BsX />
-                      </Button>
-                    </div>
-                  </Col>
-                ))}
-
-              {selectedProducts.length > 2 &&
-                currentIndex + 2 < selectedProducts.length && (
+          <div className={styles["center-div"]}>
+          {selectedImage && <Image src={selectedImage} alt="Selected product image" className="mb-3" fluid />}
+            <Form>
+              <Row className="align-items-center justify-content-center">
+                {selectedProducts.length > 2 && currentIndex > 0 && (
                   <Button
-                    onClick={handleNextClick}
+                    onClick={handlePrevClick}
                     className={styles["arrow-button"]}
                   >
-                    <FaChevronRight />
+                    <FaChevronLeft />
                   </Button>
                 )}
-            </Row>
+                {selectedProducts
+                  .slice(currentIndex, currentIndex + 2)
+                  .map((product) => (
+                    <Col key={product.id} sm={4}>
+                      <div
+                        key={product.id}
+                        className={styles["product-post-card"]}
+                      >
+                        <Image src={product.image} alt={product.name} fluid />
+                        <h5>{product.name}</h5>
+                        <Button
+                          variant="danger"
+                          onClick={() => handleRemoveProduct(product.id)}
+                          className={styles["remove-button"]}
+                        >
+                          <BsX />
+                        </Button>
+                        <Form.Check
+                        type="checkbox"
+                        label="Select"
+                        onChange={() => handleCheckboxChange(product)}
+                        />
+                      </div>
+                    </Col>
+                  ))}
 
-            <Form.Control
-              type="text"
-              placeholder="Add a title for your post"
-              className="mb-3 input-border"
-            />
-            <Form.Control
-              as="textarea"
-              rows={6}
-              className="mb-3 input-border"
-              placeholder="Write a description on what this miracle combination of products has done for you."
-            />
-            <Button
-              variant="primary"
-              type="submit"
-              className={styles["post-create-button"]}
-            >
-              Create Post
-            </Button>
-          </Form>
+                {selectedProducts.length > 2 &&
+                  currentIndex + 2 < selectedProducts.length && (
+                    <Button
+                      onClick={handleNextClick}
+                      className={styles["arrow-button"]}
+                    >
+                      <FaChevronRight />
+                    </Button>
+                  )}
+              </Row>
+
+              <Form.Control
+                type="text"
+                placeholder="Add a title for your post"
+                className="mb-3 input-border"
+              />
+              <Form.Control
+                as="textarea"
+                rows={6}
+                className="mb-3 input-border"
+                placeholder="Write a description on what this miracle combination of products has done for you."
+              />
+              <Button
+                variant="primary"
+                type="submit"
+                className={styles["post-create-button"]}
+              >
+                Create Post
+              </Button>
+            </Form>
+          </div>
         </Col>
         <Col sm={6}>
-          <BodySytemPanel
-            selectedBodySystems={selectedBodySystems}
-            toggleBodySystem={toggleBodySystem}
-          />
-          <ProductsPanel
-            selectedBodySystems={selectedBodySystems}
-            onAddProduct={handleAddProduct}
-          />
+          <div className={styles["center-div"]}>
+            <BodySytemPanel
+              selectedBodySystems={selectedBodySystems}
+              toggleBodySystem={toggleBodySystem}
+            />
+            <ProductsPanel
+              selectedBodySystems={selectedBodySystems}
+              onAddProduct={handleAddProduct}
+            />
+          </div>
         </Col>
       </Row>
     </Container>
