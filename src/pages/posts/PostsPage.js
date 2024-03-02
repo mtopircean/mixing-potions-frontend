0730d1bd9a1cb0a41a71a79be7356dcabfabdf36
Marius Topircean <mtopircean@yahoo.com>
2024-03-02 10:31:03 +0000

    Create function to sort and calculate likes ranking in front page, implemented in right panel in front

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -5,33 +5,53 @@ import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Asset from "../../components/Asset";
 import BodySystemPanel from "../../components/BodySystemPanel";
-
+import { FaThumbsUp } from "react-icons/fa";
+import userEvent from "@testing-library/user-event";
 
 function PostsPage({ message, filter = "" }) {
-    const [posts, setPosts] = useState({ results: [] });
-    const [hasLoaded, setHasLoaded] = useState(false);
-    const { pathname } = useLocation();
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-          try {
-            const { data } = await axiosReq.get(`/posts/?${filter}`);
-            setPosts(data);
-            setHasLoaded(true);
-          } catch (err) {
-            console.log(err);
-          }
-        };
-    
-        setHasLoaded(false);
-        fetchPosts();
-      }, [filter, pathname]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchPosts();
+  }, [filter, pathname]);
+
+  const sortByLikes = (posts) => {
+    const userLikesCount = {};
+    posts.forEach((post) => {
+      if (!userLikesCount[post.owner]) {
+        userLikesCount[post.owner] = 0;
+      }
+      userLikesCount[post.owner] += post.likes_count || 0;
+    });
+  
+    const sortedUsers = Object.keys(userLikesCount)
+      .filter((user) => userLikesCount[user] > 0)
+      .sort((a, b) => userLikesCount[b] - userLikesCount[a]);
+  
+    return sortedUsers.map((user) => ({
+      owner: user,
+      likes_count: userLikesCount[user],
+    }));
+  };
 
   return (
     <Row className="h-100">
-        <Col className="py-2 p-0 p-lg-2" lg={3}>
-            <BodySystemPanel />
-        </Col>
+      <Col className="py-2 p-0 p-lg-2" lg={3}>
+        <BodySystemPanel />
+      </Col>
       <Col className="py-2 p-0 p-lg-2" lg={6}>
         {hasLoaded ? (
           <>
@@ -41,7 +61,7 @@ function PostsPage({ message, filter = "" }) {
               ))
             ) : (
               <Container>
-                <h5>No result</h5>>
+                <h5>No result</h5>
               </Container>
             )}
           </>
@@ -52,10 +72,19 @@ function PostsPage({ message, filter = "" }) {
         )}
       </Col>
       <Col className="py-2 p-0 p-lg-2" lg={3}>
-        Popular profiles will sit here
+        {hasLoaded && posts.results.length > 0 && (
+          <Container>
+            <h5><FaThumbsUp /> Most liked users:</h5>
+            {sortByLikes(posts.results).map((post) => (
+              <div key={post.id}>
+                <p>{post.owner} liked {post.likes_count} times</p>
+              </div>
+            ))}
+          </Container>
+        )}
       </Col>
     </Row>
   );
 }
 
-export default PostsPage
+export default PostsPage;
