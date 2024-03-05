a27391bfac908ec54ba09dec04de7c64e7e44d20
Marius Topircean <mtopircean@yahoo.com>
2024-03-05 21:25:53 +0000

    Change PostPage so it does not pull the post, but the post elements

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -5,6 +5,7 @@ import styles from "../../styles/PostPage.module.css";
 import { faPenSquare, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useParams } from "react-router-dom";
+import { Row, Col, Button } from "react-bootstrap";
 
 const PostPage = () => {
   const [post, setPost] = useState([null]);
@@ -36,20 +37,48 @@ const PostPage = () => {
 
   return (
     <div>
-    {post && (
-      <div>
-        <Post {...post} />
-        <div>
-        <button onClick={handleEdit} className={styles["post-edit-button"]}>
-          Edit <FontAwesomeIcon icon={faPenSquare} />
-        </button>
-        <button onClick={handleDelete} className={styles["post-delete-button"]}>
-          Delete <FontAwesomeIcon icon={faTrashAlt} />
-        </button>
-      </div>
+      <Row>
+        <Col md={6}>
+          <div>
+            <Button onClick={handleEdit} className={styles["post-edit-button"]}>
+              Edit <FontAwesomeIcon icon={faPenSquare} />
+            </Button>
+            <Button
+              onClick={handleDelete}
+              className={styles["post-delete-button"]}
+            >
+              Delete <FontAwesomeIcon icon={faTrashAlt} />
+            </Button>
+          </div>
+          <div>
+            {post.image && (
+              <img
+                src={post.image}
+                alt="Post Image"
+                className="img-fluid"
+                style={{ maxWidth: "400px", maxHeight: "400px" }}
+              />
+            )}
+          </div>
+        </Col>
+        <Col md={6}>
+          <h4>Description</h4>
+          <p>{post.title}</p>
+          <div>
+            {post.comments &&
+              post.comments.map((comment, index) => (
+                <div key={index}>
+                  <p> {comment.owner}</p>
+                  <p>{comment.comment_text}</p>
+                </div>
+              ))}
+          </div>
+
+          <p>{post.description}</p>
+          <h4>Products Used</h4>
+        </Col>
+      </Row>
     </div>
-      )}
-      </div>
   );
 };
 
