b9899568011f3b41ca89946b702555eee96528ad
Marius Topircean <mtopircean@yahoo.com>
2024-03-12 19:09:32 +0000

    Add const setup, useeffect, handleChange and handleSubmit in ProfilePasswordChange

--- src/pages/profiles/ProfilePasswordChange.js
+++ src/pages/profiles/ProfilePasswordChange.js
@@ -1,21 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Form, Row, Button } from "react-bootstrap";
+import {
+  useHistory,
+  useParams,
+} from "react-router-dom/cjs/react-router-dom.min";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const ProfilePasswordChange = () => {
- 
+  const history = useHistory();
+  const { id } = useParams();
+  const currentUser = useCurrentUser();
 
+  const [userData, setUserData] = useState({
+    new_password1: "",
+    new_password2: "",
+  });
+
+  const { new_password1, new_password2 } = userData;
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  useEffect(() => {
+    if (currentUser?.profile_id?.toString() !== id) {
+    }
+  }, [currentUser, history, id]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/password/change/", userData);
+      toast.success("Password successfully changed!");
+      history.goBack();
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
 
   return (
     <>
       <Row className="justify-content-center">
         <h4 className="mb-4 mt-4">Change your password</h4>
         <Col md={8}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="newPassword">
               <Form.Label>Password:</Form.Label>
               <Form.Control
                 type="password"
-                name="new_password"
+                value={new_password1}
+                onChange={handleChange}
+                name="new_password1"
                 placeholder="Enter new password"
               />
             </Form.Group>
@@ -23,19 +64,21 @@ const ProfilePasswordChange = () => {
               <Form.Label>Confirm New Password:</Form.Label>
               <Form.Control
                 type="password"
-                name="confirm_password"
+                value={new_password2}
+                onChange={handleChange}
+                name="new_password2"
                 placeholder="Confirm new password"
               />
             </Form.Group>
+            <Col md={8}>
+              <Button variant="primary" type="submit">
+                Change Password
+              </Button>
+              <Button variant="secondary" onClick={() => history.goBack()}>
+                Cancel
+              </Button>
+            </Col>
           </Form>
-        </Col>
-      </Row>
-      <Row className="justify-content-center">
-        <Col md={8}>
-          <Button variant="primary" type="submit">
-            Change Password
-          </Button>
-          <Button variant="secondary">Cancel</Button>
         </Col>
       </Row>
     </>
