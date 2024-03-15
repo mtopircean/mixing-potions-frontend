9f327db5a04550cfd4896ab38d84b77aa670a06f
Marius Topircean <mtopircean@yahoo.com>
2024-03-15 22:01:11 +0000

    Set condition for Follow display based on currentUser

--- src/components/Follow.js
+++ src/components/Follow.js
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "react-bootstrap";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import styles from "../styles/Follow.module.css";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const FollowButton = ({ ownerId }) => {
   const [isFollowing, setIsFollowing] = useState(false);
+  const currentUser = useCurrentUser();
 
   const handleFollowUser = async () => {
-    try {
-      const response = await axios.post("/followers/", { followed: ownerId });
-      setIsFollowing(true);
-      toast.success(`You are now following this user!`);
-    } catch (error) {
-      console.error("Error following user:", error);
+    if (!isFollowing) {
+      try {
+        const response = await axios.post("/followers/", { followed: ownerId });
+        setIsFollowing(true);
+        toast.success(`You are now following this user!`);
+      } catch (error) {
+        console.error("Error following user:", error);
+      }
+    } else {
+      toast.info("You are already following this user.");
     }
   };
 
@@ -29,14 +36,26 @@ const FollowButton = ({ ownerId }) => {
     }
   };
 
+  if (!currentUser) {
+    return null;
+  }
+
   return (
     <>
       {isFollowing ? (
-        <Button variant="secondary" onClick={handleUnfollowUser}>
+        <Button
+          variant="secondary"
+          onClick={handleUnfollowUser}
+          className={styles.following}
+        >
           Following...
         </Button>
       ) : (
-        <Button variant="primary" onClick={handleFollowUser}>
+        <Button
+          variant="primary"
+          onClick={handleFollowUser}
+          className={styles.followButton}
+        >
           Follow <FontAwesomeIcon icon={faCirclePlus} />
         </Button>
       )}
