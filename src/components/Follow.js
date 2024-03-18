8e72523f7c8e771b6bd8e770591ba2fcf70576fe
Marius Topircean <mtopircean@yahoo.com>
2024-03-18 15:37:14 +0000

    Completed move of Follow functionality in external component

--- src/components/Follow.js
+++ src/components/Follow.js
@@ -7,10 +7,14 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import styles from "../styles/Follow.module.css";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
 
 const FollowButton = ({ ownerId }) => {
   const [isFollowing, setIsFollowing] = useState(false);
   const currentUser = useCurrentUser();
+
+  useEffect(() => {
+  }, [currentUser, ownerId]);
 
   const handleFollowUser = async () => {
     if (!isFollowing) {
@@ -26,30 +30,19 @@ const FollowButton = ({ ownerId }) => {
     }
   };
 
-  const handleUnfollowUser = async () => {
-    try {
-      const response = await axios.delete(`/followers/${ownerId}`);
-      setIsFollowing(false);
-      toast.success(`You have unfollowed this user!`);
-    } catch (error) {
-      console.error("Error unfollowing user:", error);
-    }
-  };
-
   if (!currentUser) {
     return null;
   }
 
+  console.log("currentUser:", currentUser);
+
+
   return (
     <>
       {isFollowing ? (
-        <Button
-          variant="secondary"
-          onClick={handleUnfollowUser}
-          className={styles.following}
-        >
-          Following...
-        </Button>
+         <Link to={`/profile/${currentUser.pk}`} className={styles.following}>
+         Following...
+       </Link>
       ) : (
         <Button
           variant="primary"
