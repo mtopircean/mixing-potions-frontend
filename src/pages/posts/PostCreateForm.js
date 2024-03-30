ed4a098868b2f49461e38ddd1f93689905532b61
Marius Topircean <mtopircean@yahoo.com>
2024-03-30 08:33:19 +0000

    Comments added to PostCreateForm for code clarity

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -10,6 +10,7 @@ import { faUpload } from "@fortawesome/free-solid-svg-icons";
 import { useHistory } from "react-router-dom";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 function PostCreateForm() {
   const currentUser = useCurrentUser();
@@ -25,6 +26,7 @@ function PostCreateForm() {
 
   const history = useHistory();
 
+  /* Toggle selection of body system */
   const toggleBodySystem = (system) => {
     if (selectedBodySystems.includes(system)) {
       setSelectedBodySystems(
@@ -35,6 +37,7 @@ function PostCreateForm() {
     }
   };
 
+  /* Add a product to selected products for the post */
   const handleAddProduct = (product) => {
     const isProductExists = selectedProducts.some((p) => p.id === product.id);
     if (!isProductExists) {
@@ -42,22 +45,26 @@ function PostCreateForm() {
     }
   };
 
+  /* Remove a product from selected products */
   const handleRemoveProduct = (productId) => {
     setSelectedProducts((prevProducts) =>
       prevProducts.filter((product) => product.id !== productId)
     );
   };
 
+  /* Move to previous product in the selected product post area */
   const handlePrevClick = () => {
     setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0));
   };
 
+  /* Move to previous product in the selected product post area */
   const handleNextClick = () => {
     setCurrentIndex((prevIndex) =>
       Math.min(prevIndex + 1, selectedProducts.length - 2)
     );
   };
 
+  /* Handle change in custom image input by uploading user image choice */
   const handleCustomImageChange = (event) => {
     if (event.target.files.length) {
       const selectedFile = event.target.files[0];
@@ -66,6 +73,7 @@ function PostCreateForm() {
     }
   };
 
+  /* Handle form submission */
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
@@ -82,13 +90,17 @@ function PostCreateForm() {
           "Content-Type": "multipart/form-data",
         },
       });
+      /* Redirect to the newly created post */
       history.push(`/posts/${response.data.id}`);
+      toast.success("Post created successfully!");
     } catch (err) {
       if (err.response?.status !== 401) {
         setErrors(err.response?.data);
       }
     }
   };
+
+  /* Redirect to login if user is not authenticated */
   useEffect(() => {
     if (!currentUser) {
       history.push("/");
@@ -98,6 +110,7 @@ function PostCreateForm() {
   return (
     <Container>
       <Row>
+        {/* Form for Creating a Post */}
         <Col sm={6}>
           <div className={styles["center-div"]}>
             <Form
@@ -108,12 +121,12 @@ function PostCreateForm() {
             >
               {(selectedImage || customImage) && (
                 <div className="text-center">
-                <Image
-                  src={customImage || selectedImage}
-                  alt="Selected product image"
-                  className={`mb-3 ${styles["preview-image"]}`}
-                  fluid
-                />
+                  <Image
+                    src={customImage || selectedImage}
+                    alt="Selected product image"
+                    className={`mb-3 ${styles["preview-image"]}`}
+                    fluid
+                  />
                 </div>
               )}
 
@@ -192,17 +205,18 @@ function PostCreateForm() {
                 onChange={(e) => setDescription(e.target.value)}
               />
               <div style={{ display: "flex", justifyContent: "center" }}>
-              <Button
-                variant="primary"
-                type="submit"
-                className={styles["post-create-button"]}
-              >
-                Create Post
-              </Button>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  className={styles["post-create-button"]}
+                >
+                  Create Post
+                </Button>
               </div>
             </Form>
           </div>
         </Col>
+        {/* Panels for Selecting Body Systems and Products */}
         <Col sm={6}>
           <div className={styles["center-div"]}>
             <BodySystemPanel
